a3328db72efe31c402933eb15cc499cdca427b19
С79_nik <64611873+mraccesdenced@users.noreply.github.com>
2023-01-27 00:39:11 +0300

    Revert "v 0.2"
    This reverts commit 9e9c140e10d25775629258a32c382de19aadde11.

--- Politeh Parser/Politeh Parser/Form1.Designer.cs
+++ Politeh Parser/Politeh Parser/Form1.Designer.cs
@@ -28,53 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LoadFileButton = new System.Windows.Forms.Button();
-            this.LoadFileText = new System.Windows.Forms.Label();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.SuspendLayout();
-            // 
-            // LoadFileButton
-            // 
-            this.LoadFileButton.Location = new System.Drawing.Point(51, 79);
-            this.LoadFileButton.Name = "LoadFileButton";
-            this.LoadFileButton.Size = new System.Drawing.Size(151, 23);
-            this.LoadFileButton.TabIndex = 0;
-            this.LoadFileButton.Text = "Загрузить Excel Файл";
-            this.LoadFileButton.UseVisualStyleBackColor = true;
-            this.LoadFileButton.Click += new System.EventHandler(this.LoadFileButton_Click);
-            // 
-            // LoadFileText
-            // 
-            this.LoadFileText.AutoSize = true;
-            this.LoadFileText.Location = new System.Drawing.Point(48, 117);
-            this.LoadFileText.Name = "LoadFileText";
-            this.LoadFileText.Size = new System.Drawing.Size(35, 13);
-            this.LoadFileText.TabIndex = 1;
-            this.LoadFileText.Text = "label1";
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.LoadFileText);
-            this.Controls.Add(this.LoadFileButton);
-            this.Name = "Form1";
             this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Button LoadFileButton;
-        private System.Windows.Forms.Label LoadFileText;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
