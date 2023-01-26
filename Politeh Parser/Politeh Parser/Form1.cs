0f6351086b11019262cb625f0b4f86e1711251dd
TwelveStein <ilyaprudenko@mail.ru>
2023-01-27 01:20:15 +0300

    v 0.3
    add function OpenFileDialog

--- Politeh Parser/Politeh Parser/Form1.cs
+++ Politeh Parser/Politeh Parser/Form1.cs
@@ -16,5 +16,13 @@ namespace Politeh_Parser
         {
             InitializeComponent();
         }
+
+        private void LoadFileButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+            LoadFileText.Text = openFileDialog1.FileName;
+            }
+        }
     }
 }
